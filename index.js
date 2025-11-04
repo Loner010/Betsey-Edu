b4573dcc47ad9afcee477a4ab71931bdc8874750
Evelyne Kayisinga <104898081+Loner010@users.noreply.github.com>
2025-11-04 14:46:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+// Manual controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Main slideshow function
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  document.getElementById("caption").innerHTML = dots[slideIndex-1].alt;
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+
+// Auto slide every 2 seconds
+setInterval(() => {
+  plusSlides(1);
+}, 2000);
